fc86ae163569ab0eb0b33411cec2647ec39bef8a
Luana Latte <luana.latte.cat@gmail.com>
2026-03-26 00:37:42 -0300

    zen browser as default

--- home/luana/browser.nix
+++ home/luana/browser.nix
@@ -2,4 +2,10 @@
 
 {
   home.packages = [ inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default ];
+
+  xdg.mimeApps.defaultApplications = {
+    "text/html" = "zen.desktop";
+    "x-scheme-handler/http" = "zen.desktop";
+    "x-scheme-handler/https" = "zen.desktop";
+  };
 }
