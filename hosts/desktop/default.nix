5c1d4462ea23b4d8581ae6f34549d810a48d2ce9
Luana Latte <luana.latte.cat@gmail.com>
2026-03-26 11:45:23 -0300

    color aliases

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -89,7 +89,9 @@
   };
 
   environment.shellAliases = {
-    ls = "ls -lh --color=always";
+    grep = "grep --color=auto";
+    diff = "diff --color=auto";
+    ls = "ls -lh --color=auto";
     ll = "ls -lh";
     la = "ls -lah";
     lla = "ls -lah";
