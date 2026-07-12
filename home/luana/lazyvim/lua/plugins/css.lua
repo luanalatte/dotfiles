7cdf21952322d7721010074c98752b713887b8a0
Luana Latte <luana.latte.cat@gmail.com>
2026-07-12 15:07:19 -0300

    lazyvim: disable annoying css autocompletions

--- home/luana/lazyvim/lua/plugins/css.lua
+++ home/luana/lazyvim/lua/plugins/css.lua
@@ -1,9 +1,35 @@
 return {
-	"neovim/nvim-lspconfig",
-	opts = {
-		servers = {
-			cssls = {
-				enabled = true,
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				cssls = {
+					enabled = true,
+				},
+			},
+		},
+	},
+	{
+		"saghen/blink.cmp",
+		opts = {
+			sources = {
+				providers = {
+					lsp = {
+						transform_items = function(ctx, items)
+							if vim.bo.filetype == "css" then
+								-- Stop annoying css property suggestions right after opening a bracket.
+
+								-- Don't show completions if the "word" before the cursor is empty.
+								-- i.e. after `{`, whitespace, or a newline.
+								if ctx.get_keyword() == "" then
+									return {}
+								end
+							end
+
+							return items
+						end,
+					},
+				},
 			},
 		},
 	},
