82c01b57cdd87f7f117c7dca3113a8bee7eeb0db
Luana Latte <luana.latte.cat@gmail.com>
2026-09-03 13:48:59 -0300

    cs tab size

--- home/luana/lazyvim/lua/config/options.lua
+++ home/luana/lazyvim/lua/config/options.lua
@@ -1,2 +1,17 @@
 vim.g.lazyvim_php_lsp = "intelephense"
 vim.g.lazyvim_python_lsp = "basedpyright"
+
+local tab_sizes = {
+  cs = 4,
+}
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    local size = tab_sizes[vim.bo.filetype]
+    if size then
+      vim.opt_local.tabstop = size
+      vim.opt_local.shiftwidth = size
+    end
+  end,
+})
