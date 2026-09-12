ffad09faa86b5fd510d0d54f024c47f9db4bd399
Luana Latte <luana.latte.cat@gmail.com>
2026-09-12 17:39:45 -0300

    fix direnv warn_timeout

--- modules/home/programs/direnv.nix
+++ modules/home/programs/direnv.nix
@@ -9,9 +9,9 @@
   programs.direnv = {
     nix-direnv.enable = lib.mkDefault true;
     nix-direnv.package = lib.mkDefault (pkgs.nix-direnv.override { nix = osConfig.nix.package; });
-    config = lib.mkDefault {
+    config = {
       global = {
-        warn_timeout = 0;
+        warn_timeout = lib.mkDefault 0;
       };
     };
   };
